3cd3b52fcb302655e9a0641ff9ca22324bab799c
Rui Liu <liurui1217@gmail.com>
2019-12-04 00:50:46 -0500

    Delete CT_TVNorm.m

--- CT_TVNorm.m
+++
@@ -1,7 +0,0 @@
-% Calculate the TV norm of an image
-% f: 2d Image
-% delta : pixel size (mono along x/y direction)
-function tvNorm = CT_TVNorm(f, delta)
-nabla_f = CTNabla(f, delta, 'xy');
-tvNorm = sum(sum(nabla_f));
-end
